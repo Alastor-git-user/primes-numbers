c28bdf5835c036fedefab6c87fdedff4b7429903
Leotroya <leotroya19@gmail.com>
2024-07-21 20:16:39 -0500

    Describe the new function

--- primos.py
+++ primos.py
@@ -1,5 +1,7 @@
 #Este es un programa para crear numeros primos 
 # esta es la version mejorada
+#creando una version que permita obtener los numeros primos hasta el
+#numero dado.
 def prime_number(n):
     numbers = list(((filter(lambda x:n%x==0, range(1,n+1)))));
     if len(numbers)==2:
