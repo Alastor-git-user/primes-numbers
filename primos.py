56ab111b5dfdb86d6db50d496bf94acc44bd0e5a
Leotroya <leotroya19@gmail.com>
2024-07-24 22:43:23 -0500

    Divisores

--- primos.py
+++ primos.py
@@ -4,12 +4,9 @@
 #numero dado.
 def prime_number(n):
     numbers = list(((filter(lambda x:n%x==0, range(1,n+1)))));
-    if len(numbers)==2:
-        return f'{n} es primo';
-    else:
-        return f'{n} no es primo';
+    return numbers;
         
-print(prime_number(13));
+print(prime_number(30));
 
 
     
